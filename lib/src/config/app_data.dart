2b82332a7a392f29b7d2f02a34a500d5d02fd494
Paulo <ribeiro.paulotm@gmail.com>
2024-05-29 21:39:59 -0400

    feat: tile card

---
+++ lib/src/config/app_data.dart
@@ -0,0 +1,73 @@
+import 'package:greemgrocer/src/models/item.model.dart';
+
+ItemModel apple = ItemModel(
+  description:
+      'A melhor maçã da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+  imgUrl: 'assets/fruits/apple.png',
+  name: 'Maçã',
+  price: 5.5,
+  unit: 'kg',
+);
+
+ItemModel grape = ItemModel(
+  imgUrl: 'assets/fruits/grape.png',
+  name: 'Uva',
+  price: 7.4,
+  unit: 'kg',
+  description:
+      'A melhor uva da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel guava = ItemModel(
+  imgUrl: 'assets/fruits/guava.png',
+  name: 'Goiaba',
+  price: 11.5,
+  unit: 'kg',
+  description:
+      'A melhor goiaba da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel kiwi = ItemModel(
+  imgUrl: 'assets/fruits/kiwi.png',
+  name: 'Kiwi',
+  price: 2.5,
+  unit: 'un',
+  description:
+      'O melhor kiwi da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel mango = ItemModel(
+  imgUrl: 'assets/fruits/mango.png',
+  name: 'Manga',
+  price: 2.5,
+  unit: 'un',
+  description:
+      'A melhor manga da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel papaya = ItemModel(
+  imgUrl: 'assets/fruits/papaya.png',
+  name: 'Mamão papaya',
+  price: 8,
+  unit: 'kg',
+  description:
+      'O melhor mamão da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+// Lista de produtos
+List<ItemModel> items = [
+  apple,
+  grape,
+  guava,
+  kiwi,
+  mango,
+  papaya,
+];
+
+List<String> categories = [
+  'Frutas',
+  'Grãos',
+  'Verduras',
+  'Temperos',
+  'Cereais'
+];
