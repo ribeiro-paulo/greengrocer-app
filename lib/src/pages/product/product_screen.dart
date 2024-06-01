40c7700d15d5ebb4530f72f15cf3bf5cfa6e3216
Paulo <ribeiro.paulotm@gmail.com>
2024-05-31 20:24:32 -0400

    feat: product details

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,29 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:greemgrocer/src/config/custom_colors.dart';
 import 'package:greemgrocer/src/models/item.model.dart';
+import 'package:greemgrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
   final ItemModel item;
-  const ProductScreen({super.key, required this.item});
+  final UtilsService utilsService = UtilsService();
+  ProductScreen({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(230),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(child: Image.asset(item.imgUrl)),
-          Expanded(
-              child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(50),
+          Column(
+            children: [
+              Expanded(
+                  child:
+                      Hero(tag: item.imgUrl, child: Image.asset(item.imgUrl))),
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.all(32),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(50),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade600,
+                          offset: const Offset(0, 2))
+                    ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            item.name,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            style: const TextStyle(
+                                fontSize: 27, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 70,
+                          color: Colors.red,
+                        )
+                      ],
+                    ),
+                    Text(
+                      utilsService.priceToCurrency(item.price),
+                      style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          color: CustomColor.customSwatchColor),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: SingleChildScrollView(
+                          child: Text(
+                            item.description,
+                            style: const TextStyle(height: 1.5),
+                          ),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      label: const Text("Adicionar ao carrrinho"),
+                      icon: const Icon(Icons.shopping_cart_outlined),
+                    )
+                  ],
                 ),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey.shade600, offset: const Offset(0, 2))
-                ]),
-          ))
+              ))
+            ],
+          ),
+          Positioned(
+            left: 10,
+            top: 10,
+            child: SafeArea(
+                child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(Icons.arrow_back_ios))),
+          )
         ],
       ),
     );
