1c4ffaf2ae97051de2a7e1c8335a00eda34b28fe
Paulo <ribeiro.paulotm@gmail.com>
2024-06-04 22:51:04 -0400

    feat: total price

--- lib/src/pages/cart/components/cart_tile.dart
+++ lib/src/pages/cart/components/cart_tile.dart
@@ -4,32 +4,53 @@ import 'package:greemgrocer/src/models/cart_item_model.dart';
 import 'package:greemgrocer/src/pages/common_widgets/quantity_widget.dart';
 import 'package:greemgrocer/src/services/utils_services.dart';
 
-class CartTile extends StatelessWidget {
+class CartTile extends StatefulWidget {
   final CartItemModel cartItemModel;
+  final Function(CartItemModel) remove;
+  const CartTile({
+    super.key,
+    required this.cartItemModel,
+    required this.remove,
+  });
+
+  @override
+  State<CartTile> createState() => _CartTileState();
+}
+
+class _CartTileState extends State<CartTile> {
   final UtilsService utilsService = UtilsService();
-  CartTile({super.key, required this.cartItemModel});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.white,
       margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       child: ListTile(
         leading: Image.asset(
-          cartItemModel.item.imgUrl,
+          widget.cartItemModel.item.imgUrl,
           height: 60,
           width: 60,
         ),
         title: Text(
-          cartItemModel.item.name,
+          widget.cartItemModel.item.name,
           style: const TextStyle(fontWeight: FontWeight.w500),
         ),
         trailing: QuantityWidget(
-            value: cartItemModel.quantity,
-            suffixText: cartItemModel.item.unit,
-            result: (quantity) {}),
+          value: widget.cartItemModel.quantity,
+          suffixText: widget.cartItemModel.item.unit,
+          result: (quantity) {
+            setState(() {
+              widget.cartItemModel.quantity = quantity;
+              if (quantity == 0) {
+                widget.remove(widget.cartItemModel);
+              }
+            });
+          },
+          isRemovable: true,
+        ),
         subtitle: Text(
-          utilsService.priceToCurrency(cartItemModel.totalPrice()),
+          utilsService.priceToCurrency(widget.cartItemModel.totalPrice()),
           style: TextStyle(
               color: CustomColor.customSwatchColor,
               fontWeight: FontWeight.bold),
--- lib/src/pages/common_widgets/quantity_widget.dart
+++ lib/src/pages/common_widgets/quantity_widget.dart
@@ -5,12 +5,14 @@ class QuantityWidget extends StatelessWidget {
   final int value;
   final String suffixText;
   final Function(int quantity) result;
+  final bool isRemovable;
 
   const QuantityWidget({
     super.key,
     required this.value,
     required this.suffixText,
     required this.result,
+    this.isRemovable = false,
   });
 
   @override
@@ -31,10 +33,11 @@ class QuantityWidget extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           _QuantityButton(
-            icon: Icons.remove,
-            color: Colors.grey,
+            icon:
+                !isRemovable || value > 1 ? Icons.remove : Icons.delete_forever,
+            color: !isRemovable || value > 1 ? Colors.grey : Colors.red,
             onPress: () {
-              if (value == 1) return;
+              if (value == 1 && !isRemovable) return;
               int resultCount = value - 1;
               result(resultCount);
             },
