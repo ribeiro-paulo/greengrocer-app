ff944e51e44bb6ceaf484c6526b262c2f310bef8
Paulo <ribeiro.paulotm@gmail.com>
2024-06-08 21:13:27 -0400

    feat: cahnge password dialog

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:greemgrocer/src/pages/common_widgets/custom_text_field.dart';
 import 'package:greemgrocer/src/config/app_data.dart' as app_mock;
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({super.key});
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,11 +58,72 @@ class ProfileTab extends StatelessWidget {
             readOnly: true,
           ),
           OutlinedButton(
-            onPressed: () {},
+            onPressed: () {
+              updatePassword();
+            },
             child: const Text("Atualizar senha"),
           )
         ],
       ),
     );
+  }
+
+  Future<bool?> updatePassword() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          "Atualização de senha",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha atual',
+                        isSecret: true,
+                      ),
+                      const CustomTextField(
+                        icon: Icons.lock_outline,
+                        label: 'Nova senha',
+                        isSecret: true,
+                      ),
+                      const CustomTextField(
+                        icon: Icons.lock_outline,
+                        label: 'Confirmar nova senha',
+                        isSecret: true,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {}, child: const Text("Atualizar"))
+                    ],
+                  ),
+                ),
+                Positioned(
+                    top: 5,
+                    right: 5,
+                    child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: const Icon(Icons.close),
+                    ))
+              ],
+            ),
+          );
+        });
   }
 }
