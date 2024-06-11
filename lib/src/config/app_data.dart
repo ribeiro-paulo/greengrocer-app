ce4cf524c12d6d722cd63a0ab92255702d3e7b30
Paulo <ribeiro.paulotm@gmail.com>
2024-06-10 21:06:31 -0400

    feat: order tab

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -1,5 +1,6 @@
 import 'package:greemgrocer/src/models/cart_item_model.dart';
 import 'package:greemgrocer/src/models/item.model.dart';
+import 'package:greemgrocer/src/models/order_model.dart';
 import 'package:greemgrocer/src/models/user_model.dart';
 
 ItemModel apple = ItemModel(
@@ -86,3 +87,15 @@ UserModel user = UserModel(
     phone: "9999999999",
     cpf: '12345678912',
     password: '15165416516');
+
+List<OrderModel> orders = [
+  OrderModel(
+    id: 'asd1d1d3f1esd1',
+    createdDateTime: DateTime.parse('2021-06-08 10:00:10.45'),
+    overdueDateTime: DateTime.parse('2021-06-08 11:00:10.45'),
+    items: cartItems,
+    status: 'pending_payment',
+    copyAndPaste: 'asdqwasdqdefga181b',
+    total: 0,
+  )
+];
---
+++ lib/src/pages/orders/orders_tab.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:greemgrocer/src/config/app_data.dart' as app_data;
+import 'package:greemgrocer/src/pages/orders/components/order_tile.dart';
+
+class OrdersTab extends StatelessWidget {
+  const OrdersTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Pedidos",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView.separated(
+          padding: const EdgeInsetsDirectional.all(16),
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (_, index) => OrderTile(
+                order: app_data.orders[index],
+              ),
+          separatorBuilder: (_, index) => const SizedBox(
+                height: 10,
+              ),
+          itemCount: app_data.orders.length),
+    );
+  }
+}
